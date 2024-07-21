88ee900123f910be03fdb40d7daf8efd828ba313
Ethan <ethanjupi@gmail.com>
2024-07-20 22:57:57 -0600

    Bean para entidad Factura

---
+++ src/main/java/com/dma/model/Factura.java
@@ -0,0 +1,19 @@
+
+package com.dma.model;
+
+import java.math.BigDecimal;
+
+/**
+ *
+ * @author 2020269
+ */
+
+//Se crea el Bean o Model para la entidad Factura. 
+
+public class Factura {
+    
+    private int numeroFactura;
+    private String estado;
+    private BigDecimal totalFactura;
+    private String fechaFactura;
+}
