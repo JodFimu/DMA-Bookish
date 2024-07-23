8d9c9c058f3857b06283447b8c5b6778bf8e76f0
jose moises Ramírez gaitan <jramirez-2023013@kinal.edu.gt>
2024-07-23 12:00:42 -0600

    Correccion de Compras

--- src/main/java/com/dma/service/IComprasService.java
+++ src/main/java/com/dma/service/IComprasService.java
@@ -5,7 +5,7 @@ import com.dma.model.Compras;
 import java.util.List;
 
 public interface IComprasService {
-    public void agregarComnpra(Compras Compra);
+    public void crearComnpra(Compras Compra);
 
     public List<Compras> listarCompras();
 
