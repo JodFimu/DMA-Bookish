463258a75a1fe0bdafe5829f02d490e766a82621
B3rs3rkk <diegotata2006@gmail.com>
2024-07-23 09:01:52 -0600

    agrege el service

--- src/main/java/com/dma/service/ComprasService.java
+++ src/main/java/com/dma/service/ComprasService.java
@@ -1,4 +1,41 @@
 package com.dma.service;
 
-public class ComprasService {
+import com.dma.model.Libro;
+import com.dma.util.JpaUtil;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.TypedQuery;
+
+import java.util.List;
+
+public class ComprasService implements IcComprasService {
+
+    private EntityManager em;
+
+    public ComprasService(){em = JpaUtil.getEntityManager();}
+
+    @Override
+    public void crearCompras(Compras compras) {
+        EntityTransaction transaction = em.getTransaction();
+
+        try{
+
+            transaction.begin();
+
+            em.persist(compras);
+            transaction.commit();
+
+        }catch (Exception e){
+            if(transaction.isActive()){
+                transaction.getRollbackOnly();
+            }
+            e.printStackTrace();
+
+        }
+    }
+    @Override
+    public List<Libro> listarCompras() {
+        TypedQuery<Libro> query = em.createQuery("select c from Compras c", Compras.class);
+        return query.getResultList();
+    }
 }
